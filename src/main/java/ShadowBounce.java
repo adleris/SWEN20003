d8ae47e809cccbb6ae95dc8ffa67c8f32fb51aee
Alexander Leris <alexander.leris@gmail.com>
2019-10-18 23:38:20 +1100

    renamed bounce to shadowbounce

--- src/main/java/ShadowBounce.java
+++ src/main/java/ShadowBounce.java
@@ -2,33 +2,23 @@ import bagel.*;
 import bagel.util.*;
 import java.lang.Math;
 
+/** The class to hold the Game */
 public class ShadowBounce extends AbstractGame {
 
-    /* the number of pegs to create */
-    private static final int NUM_PEGS = 50;
-    /* the number of pegs left on the screen */
-    private int numPegsRemaining;
-
-    /* store the game items */
-    private Peg[] pegs;
-    private Ball ball;
-    private PowerUp pu;
+    private Board board;
+    private int boardNum;
 
     /**
      * Game constructor
      */
     public ShadowBounce() {
-        ball = new Ball(new Vector2(0, 0));
-        pegs = new Peg[NUM_PEGS];
-        for (int i = 0; i < NUM_PEGS; i++) {
-            pegs[i] = new Peg();
-        }
-        numPegsRemaining = NUM_PEGS;
-        pu = new PowerUp();
+        boardNum = 0;
+        board = new Board(boardNum);
     }
 
     /**
      * Start the show
+     * @param args command line args
      */
     public static void main(String args[]) {
         ShadowBounce game = new ShadowBounce();
@@ -38,89 +28,42 @@ public class ShadowBounce extends AbstractGame {
     /**
      * Game loop: Update the ball's velocity and run collision checks. Initialises
      * the ball if need be.
+     * @param input user input
      */
     @Override
     public void update(Input input) {
 
-        /* Check if there is a ball initialised */
-        if (!ball.isOnScreen()) {
-            /*
-             * check if a new one needs to be made NB: This needs to be 2 separate
-             * conditions, there was a bug where when these were in one condition an
-             * invisible ball would destroy some pegs at the start
-             */
-            if (input.isDown(MouseButtons.LEFT)) {
-                Vector2 mousePos = input.getMousePosition().asVector();
-                ball = new Ball(velocityFromMouse(mousePos));
-                ball.setOnScreen(true);
-            }
+        /* let the board update other moving entities and check collisions
+         * Here, we parse the input from user so Board doesn't need to.
+         */
+        if (input.isDown(MouseButtons.LEFT)) {
+            Vector2 mousePos = input.getMousePosition().asVector();
+            board.update(velocityFromMouse(mousePos));
         } else {
-            /*
-             * the ball is already on the screen: adjust its acceleration according to
-             * gravity, then move it
-             */
-            ball.setVelocity(ball.getVelocity().add(new Vector2(0, Ball.gravityAcceleration)));
-            ball.moveBy(ball.getVelocity());
+            board.update(null);
         }
 
-        pu.moveBy(pu.getVelocity());
-        System.out.println(pu.getVelocity());
-        System.out.println(pu.getPosition());
+        // test to advance Boards
+        if (board.shouldEndBoard()) {
+            boardNum++;
+            if (boardNum > 4) {
+                Window.close();
+            } else {
+                board = new Board(boardNum);
+            }
+        }
 
         if (input.wasPressed(Keys.ESCAPE)) {
             Window.close();
         }
 
-        /* see if the ball hit anything */
-        checkCollisions();
-
-        /* render everything to the screen */
-        renderToScreen();
-    }
-
-    /**
-     * Check if the ball has collided with any of the remaining pegs, end the game
-     * if there are none left.
-     */
-    private void checkCollisions() {
-        /* check if there is a collision between the ball and a peg */
-        for (Peg peg : pegs) {
-            if (ball.getRectangle().intersects(peg.getRectangle()) && !peg.isDestroyed()) {
-                peg.destroy();
-                numPegsRemaining--;
-            }
-        }
-
-        /* if there are no pegs left we should end the game */
-        if (numPegsRemaining <= 0) {
-            Window.close();
-        }
-    }
-
-    /**
-     * Render All entities and score to the screen
-     */
-    public void renderToScreen() {
-        if (ball.isOnScreen()) {
-            ball.draw();
-        }
-        // temporary: render powerup and a red peg at the destination so we can see if it works
-        pu.draw();
-        Image img = new Image("res/red-peg.png");
-        img.draw(pu.destination.x, pu.destination.y);
-        // end temp. Make sure to set destination back to private
-        for (int i = 0; i < NUM_PEGS; i++) {
-            if (!pegs[i].isDestroyed()) {
-                pegs[i].draw();
-            }
-        }
     }
 
     /**
      * Find the initial velocity of the ball from the mouse coordinates
-     * 
-     * @param mouse
-     * @return
+     *
+     * @param mouse the vector of the mouse input
+     * @return the velocity vector of the ball to that mouse
      */
     public static Vector2 velocityFromMouse(Vector2 mouse) {
         double distance = Math.sqrt((mouse.x - Ball.DEFAULT_X) * (mouse.x - Ball.DEFAULT_X)
